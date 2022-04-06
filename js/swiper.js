8a4a49cbe59d7343e8014be25d9b8c143268897d
mooc1002 <76102510+mooc1002@users.noreply.github.com>
2022-04-06 19:15:54 +0900

    Add files via upload
    shadowの取り消し

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,14 @@
 
   var swiper = new Swiper(".mySwiper", {
         effect: "cube",
-    	speed: 500,
+      	speed: 500,
         grabCursor: true,
         cubeEffect: {
           loop: true,
-          shadow: true,
+          shadow: false,
           slideShadows: true,
-          shadowOffset: 20,
-          shadowScale: 0.94,
+       //   shadowOffset: 20,
+        //  shadowScale: 0.94,
 		  touchRatio: 3,
 		},
         autoplay: {
